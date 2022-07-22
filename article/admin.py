dbe6f7474b16a8eda15d421965c92f4d9de946fe
Abdulkhay_Payziev <abdulkhay.payziev@gmail.com>
2022-07-22 17:10:36 +0500

    models registred

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from article.models import Article, Comment, Tag, Bookmark
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Bookmark)
